8c3fbb658387f598c67d0458755112be2996f78e
erickyuri8 <erickyuri8@gmail.com>
2026-06-23 23:24:32 -0300

    adding images of background connecting menu and game classes

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import pygame
 
-print("Setup started")
-pygame.init()
-window = pygame.display.set_mode(size=(800, 600))
-print("Setup ended")
+from code.game import Game
 
-print("Loop started")
-while True:
-    # Check for all events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit() #close window
-            quit() #end pygame
+game = Game()
+game.run()
